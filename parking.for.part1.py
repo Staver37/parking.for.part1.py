da0e18a2bf7ce022a594b908d88c7a99f2733b37
Staver37 <102354895+Staver37@users.noreply.github.com>
2022-04-26 19:03:27 +0300

    Add files via upload

---
+++ parking.for.part1.py
@@ -0,0 +1,14 @@
+from os import system
+system("clear")
+PARKING_PLACES = 7
+FREE_PLACE = 3
+
+print("#"*PARKING_PLACES*5)
+
+for place_index in range(1, PARKING_PLACES + 1):
+    if place_index in range(3,4):
+         print("|   |", end="")
+    else:
+         print("| X |", end="")
+
+print("\n","#"*PARKING_PLACES*5, sep="")
